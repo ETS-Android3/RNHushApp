54d7effc8113e9aff08bc2ba286c52abf2f6d0f8
k0s02oi <krishnamurthy.sannidhi@walmart.com>
2022-02-25 07:01:11 -0600

    updated app js file

--- App.js
+++ App.js
@@ -7,28 +7,13 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  LogBox,
-} from 'react-native';
-import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
+import {StatusBar, LogBox} from 'react-native';
 import RootNavigator from './navigation/RootNavigator';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
   LogBox.ignoreLogs([
-    "[react-native-gesture-handler] Seems like you\'re using an old API with gesture components, check out new Gestures system!",
+    "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
   ]);
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   return (
     <>
@@ -37,24 +22,5 @@ const App = () => {
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
