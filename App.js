0df5da3c8ad3dcb953e675d7e9e83d3206e2aec2
k0s02oi <krishnamurthy.sannidhi@walmart.com>
2022-03-04 22:51:54 -0600

    implemented splash screen code changes

--- App.js
+++ App.js
@@ -6,11 +6,15 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StatusBar, LogBox} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import RootNavigator from './navigation/RootNavigator';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   LogBox.ignoreLogs([
     "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
   ]);
